1ff5bb1329e762accdbde081de49cb00073f2d32
Yusuf Eka <yusufblegoh@gmail.com>
2016-03-31 16:48:17 +0700

    Adding Products Model

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -9,9 +9,57 @@
 namespace App\Models;
 
 
+use Illuminate\Support\Collection;
+use Ixudra\Curl\Facades\Curl;
 
 class Products
 {
+    private $collection;
 
+    public function __construct()
+    {
+        $this->collection = new Collection();
+        $respons = $this->getCurl();
+        foreach ($respons as $respon){
+            $product = new Product();
+            $awal = strpos($respon,'<h5>')+4;
+            $akhir = strpos($respon,'</h5>');
+            $product->name = substr($respon,$awal,$akhir - $awal);
+            $respon = str_replace('<h5>'.$product->name.'</h5>','',$respon);
+            $awal = strpos($respon,"img src='")+9;
+            $akhir = strpos($respon,"' />");
+            $product->photo = substr($respon,$awal,$akhir - $awal);
+            $awal = strpos($respon,"<h5 class='price'>IDR ")+21;
+            $akhir = strpos($respon,".00 </h5>");
+            $product->price = substr($respon,$awal,$akhir - $awal);
+            $awal = strpos($respon,"<h5 class='price'>IDR ")+21;
+            $akhir = strpos($respon,".00 </h5>");
+            $this->collection->push($product);
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getCurl()
+    {
+        $respon = Curl::to('http://divishoes.com/main/katalog_product_ready')->get();
+        $pos = strpos($respon, '<div id="product-list">');
+        $respon = substr($respon, $pos);
+        $pos = strpos($respon, '</div>');
+        $respon = substr($respon, 0, $pos);
+        $pos = strpos($respon,'<li>');
+        $respon = substr($respon, $pos);
+        $pos = strpos($respon, '<div class="clearfix">');
+        $respon = trim(substr($respon, 0, $pos));
+        $respon = substr($respon, 0, strlen($respon)-5);
+        $respons = explode('</li>', $respon);
+        return $respons;
+    }
+
+    public function getAll()
+    {
+        return $this->collection;
+    }
 
 }
