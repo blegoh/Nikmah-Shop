854d2ca2c514ad204da07b6ee23d96b3d39de4c8
Yusuf Eka <yusufblegoh@gmail.com>
2016-03-31 16:46:47 +0700

    Adding Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,5 +12,24 @@ namespace App\Models;
 class Product
 {
 
-    
+    private $name;
+    private $description;
+    private $weight;
+    private $price;
+    private $photo;
+    private $link;
+
+    public function __set($name, $value)
+    {
+        if(property_exists($this,$name)){
+            $this->$name = $value;
+        }
+    }
+
+    public function __get($name)
+    {
+        if(property_exists($this,$name)){
+            return $this->$name;
+        }
+    }
 }
