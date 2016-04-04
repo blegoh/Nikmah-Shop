21f6477a71efa4105fc0bae9d835a91cafccc425
Yusuf Eka <yusufblegoh@gmail.com>
2016-04-04 08:00:38 +0700

    edit supplier seeder

--- database/seeds/SupplierSeeder.php
+++ database/seeds/SupplierSeeder.php
@@ -11,6 +11,17 @@ class SupplierSeeder extends Seeder
      */
     public function run()
     {
-        
+        \App\Models\Supplier::create([
+            'name' => 'Divishoes',
+            'address' => 'Tangerang',
+            'phone' => '087774837514',
+            'website' => 'http://divishoes.com'
+        ]);
+        \App\Models\Supplier::create([
+            'name' => 'Ginshashop',
+            'address' => '',
+            'phone' => '0857-1009-7162',
+            'website' => 'http://ratuwedges.com'
+        ]);
     }
 }
