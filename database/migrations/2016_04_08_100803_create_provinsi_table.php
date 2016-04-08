5d957dc69159693bab83bc6843199c99cc0bd6e4
Yusuf Eka <yusufblegoh@gmail.com>
2016-04-08 20:30:59 +0700

    add/remove more migration

---
+++ database/migrations/2016_04_08_100803_create_provinsi_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProvinsiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provinsi', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('provinsi');
+    }
+}
---
+++ database/migrations/2016_04_08_121236_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->string('email')->primary();
+            $table->string('password');
+            $table->unsignedInteger('member_id');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->foreign('member_id')->references('id')->on('members')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
