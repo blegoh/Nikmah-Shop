a52494409860900dfd314f5eef1d4327b35a06f0
Yusuf Eka <yusufblegoh@gmail.com>
2016-04-08 08:01:07 +0700

    clear banners

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,18 +55,7 @@
 					</div>
 				</div>
 				<!-- Slider Section Ends -->
-				<!-- 2 Column Banners Starts -->
-				<div class="col2-banners">
-					<ul class="row list-unstyled">
-						<li class="col-sm-8">
-							<img src="/images/banners/2col-banner1.jpg" alt="banners" class="img-responsive" />
-						</li>
-						<li class="col-sm-4">
-							<img src="/images/banners/2col-banner2.jpg" alt="banners" class="img-responsive" />
-						</li>
-					</ul>
-				</div>
-				<!-- 2 Column Banners Ends -->
+
 				<!-- Latest Products Starts -->
 				<section class="product-carousel">
 					<!-- Heading Starts -->
