cec39341cc8de8799cc6544bd4554272aafe7f0c
Yusuf Eka <yusufblegoh@gmail.com>
2016-04-08 07:43:46 +0700

    clear sidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,60 +27,6 @@
 					<!-- Special Product #5 Ends -->
 				</ul>
 				<!-- Special Products Ends -->
-				<!-- Banner #1 Starts -->
-				<img src="/images/banners/side-banner1.jpg" alt="Side Banner" class="img-responsive" />
-				<br>
-				<!-- Banner #1 Ends -->
-				<!-- Shopping Options Starts -->
-				<h3 class="side-heading">Shopping Options</h3>
-				<div class="list-group">
-					<div class="list-group-item">
-						Brands
-					</div>
-					<div class="list-group-item">
-						<div class="filter-group">
-							<label class="checkbox">
-								<input name="filter1" type="checkbox" value="br1" checked="checked" />
-								Brand Name 1
-							</label>
-							<label class="checkbox">
-								<input name="filter2" type="checkbox" value="br2" />
-								Brand Name 2
-							</label>
-							<label class="checkbox">
-								<input name="filter3" type="checkbox" value="br3" />
-								Brand Name 3
-							</label>
-							<label class="checkbox">
-								<input name="filter4" type="checkbox" value="br4" />
-								Brand Name 4
-							</label>
-						</div>
-					</div>
-					<div class="list-group-item">
-						Manufacturer
-					</div>
-					<div class="list-group-item">
-						<div class="filter-group">
-							<label class="radio">
-								<input name="filter-manuf" type="radio" value="mr1" checked="checked" />
-								Manufacturer Name 1
-							</label>
-							<label class="radio">
-								<input name="filter-manuf" type="radio" value="mr2" />
-								Manufacturer Name 2
-							</label>
-							<label class="radio">
-								<input name="filter-manuf" type="radio" value="mr3" />
-								Manufacturer Name 3
-							</label>
-						</div>
-					</div>
-					<div class="list-group-item">
-						<button type="button" class="btn btn-black">Filter</button>
-					</div>
-				</div>
-				<!-- Shopping Options Ends -->
 			</div>
 			<!-- Sidebar Ends -->
 			<!-- Primary Content Starts -->
