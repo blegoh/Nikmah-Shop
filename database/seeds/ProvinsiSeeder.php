e3e5fc8eb1f515b803c758e044e049e967014e61
Yusuf Eka <yusufblegoh@gmail.com>
2016-04-09 10:12:14 +0700

    Add provinsi seeder

---
+++ database/seeds/ProvinsiSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProvinsiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Provinsi::create(['id' => "11","name" => "Aceh"]);
+        \App\Models\Provinsi::create(['id' => "12","name" => "Sumatera Utara"]);
+        \App\Models\Provinsi::create(['id' => "13","name" => "Sumatera Barat"]);
+        \App\Models\Provinsi::create(['id' => "14","name" => "Riau"]);
+        \App\Models\Provinsi::create(['id' => "15","name" => "Jambi"]);
+        \App\Models\Provinsi::create(['id' => "16","name" => "Sumatera Selatan"]);
+        \App\Models\Provinsi::create(['id' => "17","name" => "Bengkulu"]);
+        \App\Models\Provinsi::create(['id' => "18","name" => "Lampung"]);
+        \App\Models\Provinsi::create(['id' => "19","name" => "Kepulauan Bangka Belitung"]);
+        \App\Models\Provinsi::create(['id' => "21","name" => "Kepulauan Riau"]);
+        \App\Models\Provinsi::create(['id' => "31","name" => "DKI Jakarta"]);
+        \App\Models\Provinsi::create(['id' => "32","name" => "Jawa Barat"]);
+        \App\Models\Provinsi::create(['id' => "33","name" => "Jawa Tengah"]);
+        \App\Models\Provinsi::create(['id' => "34","name" => "DI Yogyakarta"]);
+        \App\Models\Provinsi::create(['id' => "35","name" => "Jawa Timur"]);
+        \App\Models\Provinsi::create(['id' => "36","name" => "Banten"]);
+        \App\Models\Provinsi::create(['id' => "51","name" => "Bali"]);
+        \App\Models\Provinsi::create(['id' => "52","name" => "Nusa Tenggara Barat"]);
+        \App\Models\Provinsi::create(['id' => "53","name" => "Nusa Tenggara Timur"]);
+        \App\Models\Provinsi::create(['id' => "61","name" => "Kalimantan Barat"]);
+        \App\Models\Provinsi::create(['id' => "62","name" => "Kalimantan Tengah"]);
+        \App\Models\Provinsi::create(['id' => "63","name" => "Kalimantan Selatan"]);
+        \App\Models\Provinsi::create(['id' => "64","name" => "Kalimantan Timur"]);
+        \App\Models\Provinsi::create(['id' => "65","name" => "Kalimantan Utara"]);
+        \App\Models\Provinsi::create(['id' => "71","name" => "Sulawesi Utara"]);
+        \App\Models\Provinsi::create(['id' => "72","name" => "Sulawesi Tengah"]);
+        \App\Models\Provinsi::create(['id' => "73","name" => "Sulawesi Selatan"]);
+        \App\Models\Provinsi::create(['id' => "74","name" => "Sulawesi Tenggara"]);
+        \App\Models\Provinsi::create(['id' => "75","name" => "Gorontalo"]);
+        \App\Models\Provinsi::create(['id' => "76","name" => "Sulawesi Barat"]);
+        \App\Models\Provinsi::create(['id' => "81","name" => "Maluku"]);
+        \App\Models\Provinsi::create(['id' => "82","name" => "Maluku Utara"]);
+        \App\Models\Provinsi::create(['id' => "91","name" => "Papua Barat"]);
+        \App\Models\Provinsi::create(['id' => "92","name" => "Papua"]);
+    }
+}
