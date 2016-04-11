e27ec0dc048a02d3ef334161f1fedca35b4904fc
Yusuf Eka <yusufblegoh@gmail.com>
2016-04-11 23:44:08 +0700

    add product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class ProductController extends Controller
+{
+
+    public function detail($link)
+    {
+        $product = Product::find($link);
+        return $product;
+    }
+}
