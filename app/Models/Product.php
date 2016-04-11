39c6fbe2bcf0867f60ce242f435b058d56e654aa
Yusuf Eka <yusufblegoh@gmail.com>
2016-04-11 23:52:26 +0700

    Add static method to instantiate product object to show product details

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,8 @@
 namespace App\Models;
 
 
+use Ixudra\Curl\Facades\Curl;
+
 class Product extends Model
 {
 
@@ -24,6 +26,11 @@ class Product extends Model
         $this->attributes = ['name','description','weight','price','photo','link'];
     }
 
+    /**
+     * method set untuk semua property
+     * @param $name
+     * @param $value
+     */
     public function __set($name, $value)
     {
         if(property_exists($this,$name)){
@@ -31,12 +38,53 @@ class Product extends Model
         }
     }
 
+    /**
+     * method get untuk semua property
+     * @param $name
+     * @return mixed
+     */
     public function __get($name)
     {
         if(property_exists($this,$name)){
             return $this->$name;
         }
     }
-    
-    
+
+    /**
+     * @param $link
+     * @return Product
+     */
+    public static function find($link)
+    {
+        $product = new Product();
+        $product->link = $link;
+        $respon = self::getCurl($link);
+        $awal = strpos($respon,'<h3>')+4;
+        $akhir = strpos($respon,'</h3>');
+        $product->name = substr($respon,$awal,$akhir - $awal);
+        $awal = strpos($respon,'<img src="')+10;
+        $akhir = strpos($respon,'" class="image-large"/>');
+        $product->photo = substr($respon,$awal,$akhir - $awal);
+        $awal = strpos($respon,'<p>')+3;
+        $akhir = strpos($respon,'</p>');
+        $product->description = substr($respon,$awal,$akhir - $awal);
+        $awal = strpos($respon,'<input class="span3" value="')+28;
+        $akhir = strpos($respon,'" type="text" name="weight"');
+        $product->weight = substr($respon,$awal,$akhir - $awal);
+        $awal = strpos($respon,'name="price" value="')+20;
+        $akhir = strpos($respon,'.00">');
+        $product->price = substr($respon,$awal,$akhir - $awal);
+        return $product;
+    }
+
+    private function getCurl($url)
+    {
+        $respon = Curl::to($url)->get();
+        $pos = strpos($respon, '<!-- Main -->');
+        $respon = substr($respon, $pos);
+        $respon = Products::clearMetaCharacter($respon);
+        $pos = strpos($respon, '<!-- End Main -->');
+        $respon = substr($respon, 0, $pos);
+        return $respon;
+    }
 }
