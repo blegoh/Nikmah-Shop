d129c7ce103558b148dc15017d58631a86ea5b3a
Yusuf Eka <yusufblegoh@gmail.com>
2016-04-16 11:42:51 +0700

    show all product with pagination

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Paginator;
 use App\Models\Product;
-use Illuminate\Http\Request;
+use App\Models\Products;
 
 use App\Http\Requests;
 
 class ProductController extends Controller
 {
+
+    public function all($page = 1)
+    {
+        $products = new Products($page);
+        $paginator = new Paginator($products->getPage(),$page,'/products');
+        return view('products',[
+            'products' => $products->all(),
+            'paginator' => $paginator
+        ]);
+    }
 
     /**
      * Menampilkan detail dari sebuah product
