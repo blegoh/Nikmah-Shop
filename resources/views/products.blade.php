fc8e9af71b8abf399a978ca91833051f06c269b9
Yusuf Eka <yusufblegoh@gmail.com>
2016-04-16 11:11:30 +0700

    add pagination

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -2,62 +2,6 @@
 @section('primary-content')
 <!-- Primary Content Starts -->
 	<div class="col-md-9">
-	<!-- Main Heading Starts -->
-		<h2 class="main-heading2">
-			Spices &amp; Herbs
-		</h2>
-	<!-- Main Heading Ends -->
-	<!-- Category Intro Content Starts -->
-		<div class="row cat-intro">
-			<div class="col-sm-3">
-				<img src="images/misc/cat-thumb-img1.jpg" alt="Image" class="img-responsive img-thumbnail" />
-			</div>
-			<div class="col-sm-9 cat-body">
-				<p>
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-				</p>
-				<p>
-					It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-				</p>
-			</div>
-		</div>
-	<!-- Category Intro Content Ends -->
-	<!-- Product Filter Starts -->
-		<div class="product-filter">
-			<div class="row">
-				<div class="col-md-4">
-					<div class="display">
-						<a href="category-list.html">
-							<i class="fa fa-th-list" title="List View"></i>
-						</a>
-						<a href="category-grid.html" class="active">
-							<i class="fa fa-th" title="Grid View"></i>
-						</a>
-					</div>
-				 </div>
-				<div class="col-md-2 text-right">
-					<label class="control-label">Sort</label>
-				</div>
-				<div class="col-md-3 text-right">
-					<select class="form-control">
-						<option value="default" selected="selected">Default</option>
-						<option value="NAZ">Name (A - Z)</option>
-						<option value="NZA">Name (Z - A)</option>
-					</select>
-				</div>
-				<div class="col-md-1 text-right">
-					<label class="control-label" for="input-limit">Show</label>
-				</div>
-				<div class="col-md-2 text-right">
-					<select id="input-limit" class="form-control">
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3" selected="selected">3</option>
-					</select>
-				</div>
-			</div>
-		</div>
-	<!-- Product Filter Ends -->
 	<!-- Product Grid Display Starts -->
 		<div class="row">
 		<!-- Looping products -->
@@ -65,7 +9,7 @@
 			<div class="col-md-4 col-sm-6">
 				<div class="product-col">
 					<div class="image">
-						<img src="{{$product->photo}}" alt="product" class="img-responsive" />
+						<img src="{{$product->photo}}" alt="product" class="img-responsive" width="640" height="640" />
 					</div>
 					<div class="caption">
 						<h4><a href="/product/{{$product->link}}">{{$product->name}}</a></h4>
@@ -94,6 +38,7 @@
 		<!-- End of Looping products -->
 		</div>
 	<!-- Product Grid Display Ends -->
+		{!! $paginator->render() !!}
 	</div>
 <!-- Primary Content Ends -->
 @endsection
