62e438762bd7a932dc5484b28876dd46a89211de
blegoh <yusufblegoh@gmail.com>
2016-04-29 08:03:34 +0700

    update Category Seeder

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -11,8 +11,20 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Category::create(['name' => 'boot']);
-        \App\Models\Category::create(['name' => 'flat']);
-        \App\Models\Category::create(['name' => 'hells']);
+        \App\Models\Category::create([
+            'name' => 'boot & kets',
+            'site1' => 'http://ratuwedges.com/main/product_category_ready/3/category_ready',
+            'site2' => 'http://divishoes.com/main/product_category_ready/6/category_ready'
+        ]);
+        \App\Models\Category::create([
+            'name' => 'flat',
+            'site1' => 'http://ratuwedges.com/main/product_category_ready/1/category_ready',
+            'site2' => 'http://divishoes.com/main/product_category_ready/7/category_ready'
+        ]);
+        \App\Models\Category::create([
+            'name' => 'heel',
+            'site1' => 'http://ratuwedges.com/main/product_category_ready/4/category_ready',
+            'site2' => 'http://divishoes.com/main/product_category_ready/8/category_ready'
+        ]);
     }
 }
