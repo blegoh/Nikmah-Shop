8065bc75d609521e6707fe19ba75765b8957ab58
blegoh <yusufblegoh@gmail.com>
2016-05-01 12:44:19 +0700

    remove suppliers

--- resources/views/submaster.blade.php
+++ resources/views/submaster.blade.php
@@ -8,25 +8,13 @@
                 <h3 class="side-heading">Categories</h3>
                 <div class="list-group categories">
                     @foreach($categories as $category)
-                        <a href="#" class="list-group-item">
+                        <a href="/products/category/{{$category->category_id}}" class="list-group-item">
                             <i class="fa fa-chevron-right"></i>
                             {{$category->name}}
                         </a>
                     @endforeach
                 </div>
                 <!-- Categories Links Ends -->
-                <!-- Special Products Starts -->
-                <h3 class="side-heading">Suppliers</h3>
-                <ul class="side-products-list">
-                    @foreach($suppliers as $supplier)
-                        <a href="#" class="list-group-item">
-                            <i class="fa fa-chevron-right"></i>
-                            {{$supplier->name}}
-                        </a>
-                        @endforeach
-                                <!-- Special Product #5 Ends -->
-                </ul>
-                <!-- Special Products Ends -->
             </div>
             <!-- Sidebar Ends -->
             @yield('primary-content')
