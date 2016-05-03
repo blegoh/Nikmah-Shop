e3fd62a0f8619a806059502fca74ab0d1090eab8
blegoh <yusufblegoh@gmail.com>
2016-05-03 08:17:03 +0700

    add  migration to create confirm table

---
+++ database/migrations/2016_05_02_110836_create_confirms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConfirmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('confirms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('order_id');
+            $table->string('bank_account_name');
+            $table->string('bank_account');
+            $table->string('bukti_pembayaran');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('confirms');
+    }
+}
