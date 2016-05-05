e41a67bc1868844d60bafdcf669e2478a5c0d118
blegoh <yusufblegoh@gmail.com>
2016-05-05 07:30:40 +0700

    add product method

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Crypt;
 
 class OrderDetail extends Model
 {
-    
+    public function product()
+    {
+        return Product::find(Crypt::encrypt($this->product_link));
+    }
 }
