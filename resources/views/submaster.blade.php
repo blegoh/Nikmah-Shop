b42d6bfc27a7d58e749df11839a67dd4873d83c2
blegoh <yusufblegoh@gmail.com>
2016-05-05 10:31:31 +0700

    add fb fanspage

--- resources/views/submaster.blade.php
+++ resources/views/submaster.blade.php
@@ -15,6 +15,7 @@
                     @endforeach
                 </div>
                 <!-- Categories Links Ends -->
+                <div class="fb-page" data-href="https://www.facebook.com/Nikmah-SHOES-Sepatu-murah-ecer-dan-grosir-947256288679946/?__mref=message_bubble" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
             </div>
             <!-- Sidebar Ends -->
             @yield('primary-content')
