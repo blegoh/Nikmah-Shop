1d0afa4d5546c2b24282f3c684980c30c1a25dd5
blegoh <yusufblegoh@gmail.com>
2016-05-05 07:28:17 +0700

    add OrderController

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class OrderController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $member = Auth::user()->member;
+        $orders = Order::where('member_id',$member->id)->get();
+        return view('member.orders',compact('orders'));
+    }
+
+    public function detailOrder($order)
+    {
+        $order = Order::find($order);
+        return view('member.details',compact('order'));
+    }
+}
