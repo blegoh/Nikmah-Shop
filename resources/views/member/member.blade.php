2d870d798cea5837097a010774d4655e1c1b883b
blegoh <yusufblegoh@gmail.com>
2016-05-06 14:26:04 +0700

    add more field

--- resources/views/member/member.blade.php
+++ resources/views/member/member.blade.php
@@ -5,13 +5,13 @@
     <form class="form-horizontal" role="form">
         <div class="form-group">
             <label class="control-label col-sm-2" for="email">Nama:</label>
-            <div class="col-sm-10">
+            <div class="col-sm-6">
                 <input type="text" class="form-control" id="email" placeholder="Enter email" value="{{$member->name}}">
             </div>
         </div>
         <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" class="btn btn-default">Update</button>
+            <div class="col-sm-offset-2 col-sm-6">
+                <button type="submit" class="btn btn-success">Update</button>
             </div>
         </div>
     </form>
