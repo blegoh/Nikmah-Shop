3a805468e123cf0372e5fab1180cf17a66a61778
blegoh <yusufblegoh@gmail.com>
2016-05-07 06:15:29 +0700

    add logout and middleware

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -10,9 +10,16 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('guest:admin', ['except' => 'logout']);
+    }
+
     public function getLogin()
     {
         return view('admin.auth.login');
@@ -32,5 +39,11 @@ class AuthController extends Controller
         }
 
         return back()->withInput()->withError();
+    }
+
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect('/');
     }
 }
