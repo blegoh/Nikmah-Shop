0ff0cb5733beed8ba57cbbbc8509c9633437074f
blegoh <yusufblegoh@gmail.com>
2016-05-06 21:58:01 +0700

    change link to all products

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -208,7 +208,7 @@
             <div class="collapse navbar-collapse navbar-cat-collapse">
                 <ul class="nav navbar-nav">
                     <li><a href="/">Home</a></li>
-                    <li><a href="/products">All Products</a></li>
+                    <li><a href="/product/all">All Products</a></li>
                     <li><a href="/about">About</a></li>
                     <li><a href="/contact">Contact</a></li>
                     <li><a href="/contact">Panduan</a></li>
