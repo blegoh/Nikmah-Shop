5d66e02b7b6b7f98c0ace788e6a8d906d8f36d43
blegoh <yusufblegoh@gmail.com>
2016-05-08 10:24:49 +0700

    add order info

--- resources/views/member/details.blade.php
+++ resources/views/member/details.blade.php
@@ -3,6 +3,22 @@
         <!-- Primary Content Starts -->
 <div class="col-md-9">
     <h2>Order Detail</h2>
+    <dl class="dl-horizontal">
+        <dt>Nama Penerima :</dt>
+        <dd>{{$order->receiver_name}}</dd>
+        <dt>Telp Penerima :</dt>
+        <dd>{{$order->receiver_phone}}</dd>
+        <dt>Kota :</dt>
+        <dd>{{$order->city()}}</dd>
+        <dt>Alamat :</dt>
+        <dd>{{$order->ship_address}}</dd>
+        <dt>Ongkir :</dt>
+        <dd>{{$order->ongkir}}</dd>
+        @if($order->shipping_status != 'wait')
+            <dt>Resi :</dt>
+            <dd>{{$order->nomer_resi}}</dd>
+        @endif
+    </dl>
     <table class="table">
         <thead>
         <tr>
@@ -25,6 +41,16 @@
         @endforeach
         </tbody>
     </table>
+    @if($order->is_paid == 0 && $order->confirms->count() == 0)
+        <a href="/order/confirm/{{$order->id}}" class="btn btn-info">Konfirmasi Pembayaran</a>
+        <br /><br />
+    @elseif($order->is_paid == 0 && $order->confirms->count() > 0)
+        <h3>Proses Konfirmasi Pembayaran</h3>
+    @elseif($order->shipping_status == 'shipping')
+        <h3>Proses Pengiriman</h3>
+    @elseif($order->shipping_status == 'shipped')
+        <h3>Terkirim</h3>
+    @endif
 </div>
 <!-- Primary Content Ends -->
 @endsection
