4b1f564bd586937512bf5aa9d3469e5cf7cb6c66
blegoh <yusufblegoh@gmail.com>
2016-05-08 09:22:46 +0700

    add index and detail method

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -10,11 +10,24 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 
 class OrderController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth:admin');
+    }
+
+    public function index()
+    {
+        $orders = Order::all();
+        return view('admin.order',compact('orders'));
+    }
+
+    public function detail($order)
+    {
+        $order = Order::find($order);
+        return view('admin.order-detail',compact('order'));
     }
 }
