3c3722d83679ea9ad520ab55670f2fe053cfbfbe
blegoh <yusufblegoh@gmail.com>
2016-05-08 20:35:37 +0700

    add relation to member

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,6 +11,11 @@ class Order extends Model
         return $this->hasMany(OrderDetail::class,'order_id','id');
     }
 
+    public function member()
+    {
+        return $this->belongsTo(Member::class,'member_id','id');
+    }
+
     public function confirms()
     {
         return $this->hasMany(Confirm::class,'order_id','id');
