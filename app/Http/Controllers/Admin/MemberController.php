1ed63f39d819072191ad5eeaba30183fe52bc32c
blegoh <yusufblegoh@gmail.com>
2016-05-08 09:21:37 +0700

    add index method

--- app/Http/Controllers/Admin/MemberController.php
+++ app/Http/Controllers/Admin/MemberController.php
@@ -10,11 +10,19 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\Controller;
+use App\Models\Member;
 
 class MemberController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth:admin');
+    }
+
+    public function index()
+    {
+        $members = Member::all();
+        //return $members;
+        return view('admin.member',compact('members'));
     }
 }
