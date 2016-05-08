6b591cca2f8f5e6cefbe482cf1decea1c3b04484
blegoh <yusufblegoh@gmail.com>
2016-05-08 20:35:13 +0700

    add relation to user and add method to get city name

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
@@ -10,4 +11,14 @@ class Member extends Model
     protected $primaryKey = 'id';
 
     public $timestamps = false;
+
+    public function user()
+    {
+        return $this->hasOne(User::class,'member_id','id');
+    }
+
+    public function city()
+    {
+        return Ongkir::city($this->city);
+    }
 }
