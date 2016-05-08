754abb6f0306b1db220bd3be7e31f3026c7f42ca
blegoh <yusufblegoh@gmail.com>
2016-05-08 18:58:26 +0700

    add confimation method

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -11,6 +11,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
@@ -29,5 +30,27 @@ class OrderController extends Controller
     {
         $order = Order::find($order);
         return view('admin.order-detail',compact('order'));
+    }
+
+    public function confirm($order)
+    {
+        $order = Order::find($order);
+        $confirm = $order->confirms->first();
+        return view('admin.confirm',compact('confirm'));
+    }
+
+    public function updateConfirm($order,Request $request)
+    {
+        $order = Order::find($order);
+        $confirm = $order->confirms->first();
+        if ($request->input('valid') == '1')
+        {
+            $order->is_paid = 1;
+            $order->update();
+        }
+        foreach ($order->confirms as $confirm) {
+            $confirm->delete();
+        }
+        return redirect('/admin/order/'.$order->id);
     }
 }
