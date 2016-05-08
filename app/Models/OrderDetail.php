886a60d6e820b45278336b3ddcb9c71167d70a84
blegoh <yusufblegoh@gmail.com>
2016-05-08 20:36:06 +0700

    add anonymous global scope

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -3,10 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use DB;
+
 use Crypt;
 
 class OrderDetail extends Model
 {
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('total', function(Builder $builder) {
+            $builder->select(['*',DB::raw('unit_price * quantity as total')]);
+        });
+    }
+
     public function product()
     {
         return Product::find(Crypt::encrypt($this->product_link));
